03c9e783dbf9f0300a597fe1c93daa8796557c84
Ravi kumar <rkravikr9102@gmail.com>
2022-04-01 22:35:21 +0530

    Done Using Loops

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,34 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+// for(let i = 1; i <= 10; i = i + 1){
+//     console.log(i);
+// }
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+// for(let i = 1; i <= 10; i = i + 1){
+//     if(i % 2 == 0){
+//         console.log(i);
+//     }
+// }
 
 // Using `console.log` log all the od values from 1 to 10.
 
+// for(let i = 1; i <= 10; i = i + 1){
+//     if(i % 2 !== 0){
+//         console.log(i);
+//     }
+// }
+
 // Calculate the sum of all numbers from 1 to 10.
 
+let sum = 0
+
+for(let i =1; i<= 10; i++) {
+    sum = sum+i;
+}
+    console.log(i);
+
 // Log all the values from 1 to 10 using while loop
+
