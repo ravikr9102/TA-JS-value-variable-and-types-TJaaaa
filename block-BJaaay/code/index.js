93a0a11b95a6b876e221e43f3cafb2373dc7c38e
Ravi kumar <rkravikr9102@gmail.com>
2023-04-14 00:32:42 +0530

    refactor

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,41 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for(let i = 0; i <= 10; i++){
+    console.log(i)
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
+
+for(let i = 0; i <= 10; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
 
 // Using `console.log` log all the od values from 1 to 10.
 
+for(let i = 0; i <= 10; i++){
+    if(i % 2 !== 0){
+        console.log(i)
+    }
+}
+
+
 // Calculate the sum of all numbers from 1 to 10.
 
+let sum = 0;
+
+for(let i = 1; i <= 10; i++){
+    sum += i
+}
+console.log(sum)
+
 // Log all the values from 1 to 10 using while loop
+
+let value = 1;
+
+while (value <= 10) {
+    console.log(value);
+    value++
+}
