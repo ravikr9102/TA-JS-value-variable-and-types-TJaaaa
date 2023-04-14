ebcfeeae5e106626b2dfd34ffcee3d6d57067e30
Ravi kumar <rkravikr9102@gmail.com>
2023-04-14 14:33:49 +0530

    refactor

--- block-BJaabb/code/index.js
+++ block-BJaabb/code/index.js
@@ -6,21 +6,71 @@
 4. Now change the value of `user` to `John` and log the value of both `user` and `userName`.
 */
 
+let user = prompt("Enter your name");
+alert(user);
+
+let userName = user;
+console.log(userName,user);
+
+let age = 22 , isLearning =  true;
+
+user = 'john'
+
+console.log(user, userName);
+
+
 /* 2.
 Use `prompt` to take two numbers from user and store it in variable `numA` and `numB`. Create a third variable called `sum`, using `+` operator add numA and numB , store it in varible `sum` and using alert display `sum` in browser.
 */
+
+let numA = +prompt("Enter your first number");
+let numB = +prompt("Enter your second number");
+
+let sum = numA + numB;
+
+alert(`The sum of ${numA} and ${numB} is ${sum}`);
 
 // Loops
 
 // 3. Complete the following code to make the output be 0 2 4 6 8 10?
 
-for (let j = 0; j <= 10; ____) console.log(j);
+for (let j = 0; j <= 10; j+= 2){
+    console.log(j);
+} ;
 
 // 4. Print the odd numbers from 9 to 1 (both inclusive) using a for loop.
 
+for(let i = 9; i >= 1; i--){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+}
+
 // 5. Concat all the numbers from 5 to 0 (both inclusive) in descending order using a while loop. (543210)
 
+let i = 5;
+let result = '';
+
+while (i >= 0) {
+  result += i;
+  i--;
+}
+console.log(result);
+
+
 // 6. Concat all the even numbers from 10 to 0 (both inclusive) in descending order using a while loop. (1086420)
+
+let j = 10;
+let value = '';
+
+while( j >= 0){
+    if( j % 2 === 0){
+        value += j
+    }
+    j--
+}
+
+console.log(value);
 
 // Comparisoin
 
@@ -36,3 +86,12 @@ Example:
 "hello", -21; // false
 
 */
+
+let num1 = +prompt("Enter the num1")
+let num2 = +prompt("Enter the num2")
+
+if(isNan(num1) || isNaN(num2)) {
+    alert('enter a valid value!')
+} else {
+    alert(num1 === num2);
+}
